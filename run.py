eabc014281e1127ac854827205f6da7c0ef696c8
jayab2010 <jayab2010@gmail.com>
2020-07-31 03:21:50 +0000

    updated

--- run.py
+++ run.py
@@ -1,10 +1,39 @@
 import os
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
 
-@app.route('/')
+
+def add_messages(username, message):
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
+
+
+def get_all_messages():
+    """Get all of the messages and separate them with a `br`"""
+    return "<br>".join(messages)
+
+
+@app.route("/")
 def index():
-    return "<h1>hello there</h1>"
+    """Main page with instructions"""
+    return "To send a message use: /USERNAME/MESSAGE"
 
-app.run(host=os.getenv('IP'), port=init(os.getenv('PORT')), debug=True)     
+
+@app.route("/<username>")
+def user(username):
+    """Display chat messages"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
+
+
+app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
